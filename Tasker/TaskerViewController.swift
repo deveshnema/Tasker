e16363887eb61c6012bc2fb3c9ece5f35949e141
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-06-12 21:04:22 -0700

    Implemented "add new task" functionality

--- Tasker/TaskerViewController.swift
+++ Tasker/TaskerViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class TaskerViewController: UITableViewController {
 
-    let itemArray = ["travel", "read books", "buy groceries"]
+    var itemArray = ["travel", "read books", "buy groceries"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK:- Tableview datasource methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -26,6 +27,62 @@ class TaskerViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK:- Tableview delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
+    //MARK:- Add new items
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let action = UIAlertAction(title: "Add Task", style: UIAlertActionStyle.default) { (action) in
+            //what will happen once the user clicks the add item button on our alert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new task"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
