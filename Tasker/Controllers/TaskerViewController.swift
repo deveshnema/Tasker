90a4dcc385047ef8cdeebab14f819f0a0d1ce157
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-06-12 22:31:30 -0700

    1) Grouped files into MVC structure 2) Added the Task Model 3) Use the Task Model inside TaskerVC

--- Tasker/Controllers/TaskerViewController.swift
+++ Tasker/Controllers/TaskerViewController.swift
@@ -10,43 +10,55 @@ import UIKit
 
 class TaskerViewController: UITableViewController {
 
-    var itemArray = ["travel", "read books", "buy groceries"]
+    var taskArray = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fetchData()
     }
 
+    func fetchData() {
+        let travelTask = Task(title: "travel", done: false)
+        let groceriesTask = Task(title: "buy groceries", done: true)
+        let gymTask = Task(title: "go to gym", done: true)
+        
+        taskArray.append(travelTask)
+        taskArray.append(groceriesTask)
+        taskArray.append(gymTask)
+
+    }
+    
     //MARK:- Tableview datasource methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return taskArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskerItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        cell.accessoryType = task.done ? .checkmark : .none
         return cell
     }
     
     //MARK:- Tableview delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        taskArray[indexPath.row].done = !taskArray[indexPath.row].done
         tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
+
     }
 
-    //MARK:- Add new items
+    //MARK:- Add new tasks
     @IBAction func addNewItem(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: UIAlertControllerStyle.alert)
         
         let action = UIAlertAction(title: "Add Task", style: UIAlertActionStyle.default) { (action) in
-            //what will happen once the user clicks the add item button on our alert
-            self.itemArray.append(textField.text!)
+            //what will happen once the user clicks the add task button on our alert
+            let task = Task(title: textField.text!, done: false)
+            self.taskArray.append(task)
             self.tableView.reloadData()
         }
         
---
+++ Tasker/Models/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Tasker
+//
+//  Created by Devesh Nema on 6/12/18.
+//  Copyright © 2018 Devesh Nema. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    var title: String = ""
+    var done: Bool = false
+}
